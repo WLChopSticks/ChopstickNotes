01f53c8d9a63dbaef186d88bf780fd69352adb3c
WLChopSticks <wanglei_sy56@163.com>
2016-02-17 14:19:06 +0800

    初始化程序

---
+++ ChopstickNotes/ChopstickNotes/Classes/Home(首页)/ViewController/WLCHomeController.h
@@ -0,0 +1,13 @@
+//
+//  WLCHomeController.h
+//  ChopstickNotes
+//
+//  Created by 王 on 16/2/17.
+//  Copyright © 2016年 WLChopSticks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLCHomeController : UIViewController
+
+@end
---
+++ ChopstickNotes/ChopstickNotes/Classes/Main/ViewController/WLCTabBarController.m
@@ -0,0 +1,47 @@
+//
+//  WLCTabBarController.m
+//  ChopstickNotes
+//
+//  Created by 王 on 16/2/17.
+//  Copyright © 2016年 WLChopSticks. All rights reserved.
+//
+
+#import "WLCTabBarController.h"
+
+@interface WLCTabBarController ()
+
+@end
+
+@implementation WLCTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    //添加子控制器
+    
+    
+}
+
+//添加自控制器
+- (void)addChildViewControllers {
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
